61feac635306075ef26f62071cdfd31e43a26b2e
Adam Koblentz <akoblentz@gmail.com>
2011-11-06 16:21:14 -0500

    Added label and some json url boilerplate.

--- KenmoreSafe/ViewController.m
+++ KenmoreSafe/ViewController.m
@@ -6,9 +6,12 @@
 //  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
 //
 
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) //1
+#define kMLBScheduleURL [NSURL URLWithString: @"http://mlb.mlb.com/components/schedule/schedule_20110731.json"] //2
 #import "ViewController.h"
 
 @implementation ViewController
+@synthesize answerLabel;
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,10 +25,29 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    dispatch_async(kBgQueue, ^{
+        NSData* data = [NSData dataWithContentsOfURL: 
+                        kMLBScheduleURL];
+        [self performSelectorOnMainThread:@selector(fetchedData:) 
+                               withObject:data waitUntilDone:YES];
+    });
+}
+- (void)fetchedData:(NSData *)responseData {
+    //parse out the json data
+    NSError* error;
+    NSDictionary* json = [NSJSONSerialization 
+                          JSONObjectWithData:responseData //1
+                          
+                          options:kNilOptions 
+                          error:&error];
+    
+    NSArray* venues = [json objectForKey:@""];    
+    NSLog(@"loans: %@", venues); //3
 }
 
 - (void)viewDidUnload
 {
+    [self setAnswerLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
